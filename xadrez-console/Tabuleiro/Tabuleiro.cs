124a04be18a18bcdf91aed670dcbc7c95a5a8945
DayvidSS <dssinformatica@hotmail.com>
2018-06-07 22:58:23 -0300

    Melhorando a implementação do metodo imprimirTabuleiro

--- xadrez-console/Tabuleiro/Tabuleiro.cs
+++ xadrez-console/Tabuleiro/Tabuleiro.cs
@@ -26,14 +26,14 @@
 
         public void colocarPeca(Peca p, Posicao pos) {
             if (existePeca(pos)) {
-                throw new TabuleiroException ("Já existe uma peça nesta posição");
+                throw new TabuleiroException ("Já existe uma peça nessa posição");
             }
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
         }
 
         public bool posicaoValida(Posicao pos) {
-            if (pos.linha < 0 || pos.linha <= linhas || pos.coluna < 0 || pos.coluna <= colunas) {
+            if (pos.linha < 0 || pos.linha >= linhas || pos.coluna < 0 || pos.coluna >= colunas) {
                 return false;
             }
             return true;
